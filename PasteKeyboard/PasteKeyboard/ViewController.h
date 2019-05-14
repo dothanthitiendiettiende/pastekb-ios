a691b73ebf261d748eff5147bc7cea990c2341f5
everettjf <everettjf@live.com>
2019-05-15 00:10:25 +0800

    got

--- PasteKeyboard/PasteKeyboard/ViewController.h
+++ PasteKeyboard/PasteKeyboard/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PAAListViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : PAAListViewController
 
 
 @end
--- PasteKeyboard/PasteKeyboard/ViewController.m
+++ PasteKeyboard/PasteKeyboard/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <UIView+Toast.h>
 
 @interface ViewController ()
 
@@ -14,9 +15,78 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationItem.title = @"Paste Keyboard";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    NSString *shortVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    NSString *buildVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
+    NSString *appVersion = [NSString stringWithFormat:@"%@.%@",shortVersion,buildVersion];
+    
+    
+    __weak typeof(self) wself = self;
+    self.groups = @[
+                    @{
+                        @"title":@"General",
+                        @"rows" : @[
+                                @{
+                                    @"title":@"User Guide",
+                                    @"action":^(){
+                                        [wself openInBrowser:@"https://pastekeyboard.github.io"];
+                                    },
+                                    },
+                                ]
+                        },
+                    @{
+                        @"title":@"Feedback",
+                        @"rows" : @[
+                                @{
+                                    @"title":@"Email",
+                                    @"action":^(){
+                                        [wself openURL:@"mailto://everettjf@live.com"];
+                                    },
+                                    },
+                                ]
+                        },
+                    @{
+                        @"title":@"Author",
+                        @"rows" : @[
+                                @{
+                                    @"title":@"Twitter",
+                                    @"action":^(){
+                                        [wself openInBrowser:@"https://twitter.com/everettjf"];
+                                    },
+                                    },
+                                @{
+                                    @"title":@"Weibo",
+                                    @"action":^(){
+                                        [wself openInBrowser:@"https://weibo.com/everettjf"];
+                                    },
+                                    },
+                                @{
+                                    @"title":@"Blog",
+                                    @"action":^(){
+                                        [wself openInBrowser:@"https://everettjf.github.io"];
+                                    },
+                                    },
+                                ]
+                        },
+                    @{
+                        @"title":@"More",
+                        @"rows" : @[
+                                @{
+                                    @"title":[NSString stringWithFormat:@"Version : %@",appVersion],
+                                    @"action":^(){
+                                    },
+                                    },
+                                ]
+                        },
+                    ];
 }
 
 
